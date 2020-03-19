b5f571f5da3e06c07a45f0e014b920434721e157
Krystyna W <krystyna.wuczynska@vazco.eu>
2020-03-19 14:06:48 +0100

    Client changed

--- client/main.js
+++ client/main.js
@@ -1,11 +1,9 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-import { Books } from '../imports/books.js';
 
 Template.body.helpers({
   books() {
-    return {}; //Books.find({});
+    return;
   },
 });
